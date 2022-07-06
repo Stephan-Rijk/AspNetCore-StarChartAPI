457cf96e7f38d494e5fbeeff9be6c7ce9f5e49e6
Stephan-Rijk <108409281+Stephan-Rijk@users.noreply.github.com>
2022-07-06 11:06:18 +0200

    s

--- StarChart/Controllers/CelestialController.cs
+++ StarChart/Controllers/CelestialController.cs
@@ -11,11 +11,42 @@ namespace StarChart.Controllers
     [ApiController]
     public class CelestialObjectController : ControllerBase
     {
+        
         private readonly ApplicationDbContext _context;
 
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name = "GetById")]
+        public async GeyById<IActionResult>(int id)
+        {
+            var celestialObject = _context.CelestialObjects.Find(id);
+            if(CelestialObject == null)
+                return NotFound();
+            celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == id).ToList();
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name}")]
+        public async GetByName<IActionResult>(string name)
+        {
+            var celestialObject = _context.CelestialObjects.Find(name);
+            if (celestialObject == null)
+                return NotFound();
+            foreach (celestialObject)
+                celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == id).ToList();
+            return Ok(celestialObject);
+        }
+
+        [HttpGet]
+        public async GetAll<IActionResult>()
+        {
+            foreach(CelestialObject)
+                celestialObject.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == id).ToList();
+            return Ok(celestialObject);
+
         }
     }
 }
